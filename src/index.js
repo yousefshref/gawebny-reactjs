07771e5c16a0357d065905af223955d31c84cbd7
Yousef <yb2005at@gmail.com>
2023-02-22 14:13:02 +0200

    ss

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <HashRouter basename='/'>
         <App />
       </HashRouter>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
